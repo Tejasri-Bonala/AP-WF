b326414dcbbe4c63b9afbcec84eab98e32bb5386
Tejasri Bonala <tejasri.bonala@aarini.com>
2024-10-18 12:14:03 +0000

    change in status file

--- workflow-ui-module/webapp/controller/App.controller.js
+++ workflow-ui-module/webapp/controller/App.controller.js
@@ -1,29 +1,62 @@
 sap.ui.define(
-    [
-        "sap/ui/core/mvc/Controller"
-    ],
-    function(BaseController) {
-      "use strict";
+  [
+    "sap/ui/core/mvc/Controller",
+    "sap/ui/core/format/DateFormat",
+  ],
+  function (BaseController, DateFormat) {
+    "use strict";
 
-      
-  
-      return BaseController.extend("com.aperam.workflowuimodule.controller.App", {
-        determineFieldVisibility: function(accountingType, fieldType) {
-          switch (accountingType) {
-            case "Capex":
-              return fieldType === "Capex";
-            case "Opex":
-              return fieldType === "Opex";
-            case "Lease":
-              return fieldType === "Lease";
-            case "Capex/Opex":
-              return fieldType === "Capex" || fieldType === "Opex";
-            default:
-              return false;
-          }
-        },
-        onInit() {
-        },
-      });
+
+
+    return BaseController.extend("com.aperam.workflowuimodule.controller.App", {
+      determineFieldVisibility: function (accountingType, fieldType) {
+        switch (accountingType) {
+          case "Capex":
+            return fieldType === "Capex";
+          case "Opex":
+            return fieldType === "Opex";
+          case "Lease":
+            return fieldType === "Lease";
+          case "Capex/Opex":
+            return fieldType === "Capex" || fieldType === "Opex";
+          default:
+            return false;
+        }
+      },
+      onInit() {
+        //   var oFeedModel = new sap.ui.model.json.JSONModel({
+        //     EntryCollection: []
+        // });
+        // this.getView().setModel(oFeedModel, "Feed");
+      },
+      onPost: function (oEvent) {
+        var oFormat = DateFormat.getDateTimeInstance({ style: "medium" });
+        var oDate = new Date();
+        var sDate = oFormat.format(oDate);
+        var sValue = oEvent.getParameter("value");
+
+        // create new entry
+        var oEntry = {
+          Author: "User",
+          Type: "Reply",
+          Date: "" + sDate,
+          Text: sValue
+        };
+
+        // get the Feed model
+        var oModel = this.getView().getModel("Feed");
+
+        if (oModel) {
+          var aEntries = oModel.getData().EntryCollection || [];
+          aEntries.unshift(oEntry);
+          oModel.setData({
+            EntryCollection: aEntries
+          });
+        } else {
+          console.error("Feed model not found");
+        }
+      }
+
+
     });
-  
+  });
